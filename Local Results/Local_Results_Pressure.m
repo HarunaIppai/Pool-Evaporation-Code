fac59e0f62788e3694d37681819e57f6385da9b8
Nagayumi Haruna <Krn@localhost.localdomain>
2020-09-24 18:24:57 -0500

    Added Pressure Based Model

---
+++ Local Results/Local_Results_Pressure.m
@@ -0,0 +1,29 @@
+function evap = Local_Results_Pressure(Ta,Pa,Tw,Pw,gogs)
+    % The correcting factor for V=0.101 is 0.233
+    L = 1;
+    ABST = Tw;
+    ABSTA = Ta;
+    D12 = 4.14*exp(0.0061*ABST)*10^-6; % T is in terms of Kelvin, coefficient in terms of m^2/s
+    P1 = Pw;
+    P1e = Pa;
+    M2 = 29;
+    M1 = 18;
+    m1 = P1/(Pw*(M2/M1)-P1*(1-M2/M1));
+    m1s = m1;
+    m1e = P1e/(Pw*(M2/M1)-P1e*(1-M2/M1));
+    m2 = 1-m1;
+    Mavg = (m1/M1+m2/M2)^-1;
+    rho = Pw/(8.314/Mavg)/ABST;
+    Sc = 0.62;
+    rhoe = Pw/(8.314/Mavg)/ABSTA;
+    rhos = rho;
+    nyu = Sc*D12;
+    g = 9.81;
+    Gr = g*(rhoe-rhos)*L^3/rho/nyu^2;
+    Ram = Gr*Sc;
+    Sh = Ram^(1/3);
+    gs = rho*D12*Sh/L;
+    B = (m1s - m1e)/(1 - m1s);
+    evap = B*gs*gogs;
+
+end
